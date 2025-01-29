d44ff4ca31c23aea651ab9de17ba4f9eadee430c
angleshell <caiquescariot@gmail.com>
2025-01-29 20:22:09 -0300

    1.2

---
+++ aula1.4.php
@@ -0,0 +1,9 @@
+<?php
+echo "Escreve. ";
+$numero = readline();
+for ($i = 0; $i < 11; $i++) {
+    $tabuada = $numero * $i;
+    echo "$numero x $i = $tabuada\n" ;
+}
+
+?>
--- aula1.php
+++ aula1.php
@@ -1,1 +1,77 @@
-11111
+<?php 
+
+echo"Informe o numero A: ";
+
+$numeroa = (int) readline();
+
+echo"Informe o numero B: ";
+
+$numerob = (int) readline();
+
+$subtrai = $numeroa - $numerob;
+
+$adiciona = $numeroa + $numerob;
+
+$divide = $numeroa / $numerob;
+
+$multiplica = $numeroa * $numerob;
+
+echo"Informe qual ação deseja fazer: 1 = Subtração, 2 = Adição, 3 = Divisão, 4 = Multiplicação ";
+
+$escolha = 0;
+$escolha = (int) readline();
+
+If ($escolha == 1){
+
+    //subtração
+
+    echo $subtrai;
+
+    if ($subtrai % 2 == 0) {
+
+        echo " é um numero par";
+    }
+        else {
+            echo " é um numero impar";
+        }
+    }
+
+    elseif ($escolha == 2){
+    
+    //adição
+
+    echo $adiciona;
+        if ($adiciona % 2 == 0){
+            echo " é um numero par";
+        }
+        else {
+            echo " é um numero impar";
+        }
+    }
+
+    elseif ($escolha == 3){
+
+    //divisão
+
+    echo $divide;
+        if ($divide % 2 == 0){
+            echo " é um numero par";
+        }
+        else {
+            echo " é um numero impar";
+        }
+    }
+
+    elseif ($escolha == 4){
+    
+    //multiplicação
+
+    echo $multiplica;
+        if ($multiplica % 2 == 0){
+            echo " é um numero par";
+        }
+        else {
+            echo " é um numero impar";
+        }
+    }
+?>
