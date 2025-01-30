f6ffd2d8455f74cc8519d774e2567a269ae6bb9f
angleshell <caiquescariot@gmail.com>
2025-01-29 21:46:13 -0300

    1.2

--- aula1.5.php
+++ aula1.5.php
@@ -1,11 +1,10 @@
 <?php
-//$numero = rand(1,100);
-$numero = 2;
+$numero = rand(1,100);
 for($attempt = 1 ; $attempt < 6 ; $attempt++){
     echo "Digite um numero: \n";
     $guess = readline();
     if ($guess == $numero){
-        echo "Parabens! você acertou";
+        echo "Parabens! você acertou!";
         break;
     }
     else{
